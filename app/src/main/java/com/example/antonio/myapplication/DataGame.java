ac8573ccbf87569b33a56cb4334cefc49c4eb0fb
Antonio Segovia Pastor <asegovia024@gmail.com>
2018-07-22 20:45:20 +0200

    Mejora del DataGame

--- app/src/main/java/com/example/antonio/myapplication/DataGame.java
+++ app/src/main/java/com/example/antonio/myapplication/DataGame.java
@@ -8,14 +8,53 @@ public class DataGame {
 
     String nombrep1, nombrep2;
     int sexp1, sexp2; //1 hombre 2 mujer
+    int puntos1, puntos2;
+    int nivel1, nivel2;
 
 
-    DataGame(String n1, String n2, int s1, int s2){
-        nombrep1=n1;
-        nombrep2=n2;
-        sexp1=s1;
-        sexp2=s2;
+    public DataGame(String nombrep1, String nombrep2, int sexp1, int sexp2 ) {
+        this.nombrep1 = nombrep1;
+        this.nombrep2 = nombrep2;
+        this.sexp1 = sexp1;
+        this.sexp2 = sexp2;
+        this.puntos1 = 0;
+        this.puntos2 = 0;
+        this.nivel1 = 0;
+        this.nivel2 = 0;
     }
+
+    public int getPuntos1() {
+        return puntos1;
+    }
+
+    public void setPuntos1(int puntos1) {
+        this.puntos1 = puntos1;
+    }
+
+    public int getPuntos2() {
+        return puntos2;
+    }
+
+    public void setPuntos2(int puntos2) {
+        this.puntos2 = puntos2;
+    }
+
+    public int getNivel1() {
+        return nivel1;
+    }
+
+    public void setNivel1(int nivel1) {
+        this.nivel1 = nivel1;
+    }
+
+    public int getNivel2() {
+        return nivel2;
+    }
+
+    public void setNivel2(int nivel2) {
+        this.nivel2 = nivel2;
+    }
+
 
     public String getNombrep1() {
         return nombrep1;
